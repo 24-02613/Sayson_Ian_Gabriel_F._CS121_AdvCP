d2bf8563365c3bfeade15d99d91da2640abef250
24-02613 <24-02613@g.batstate-u.edu.ph>
2025-02-13 12:13:51 +0800

    Add files via upload

--- Laboratory Activities/Laboratory Activity 1 – 13-2-2025/perfect_number.py
+++ Laboratory Activities/Laboratory Activity 1 – 13-2-2025/perfect_number.py
@@ -1,1 +1,11 @@
+number = int(input("Enter a number: "))
+divisor = 0
 
+for i in range (1, number): 
+    if number % i == 0:
+        divisor += i
+
+if divisor == number:
+    print (f"{number} is a perfect number")
+else:
+    print (f"{number} is not a perfect number.")
